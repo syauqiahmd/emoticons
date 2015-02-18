80b3eb816e53bc0051324532ad5a4ba38862dc7f
Connor Peet <connor@peet.io>
2015-02-18 13:10:45 -0600

    Simple auto-loader thing

---
+++ index.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var output = module.exports = {};
+
+fs.readdirSync(__dirname).map(function (dir) {
+    var path = __dirname + '/' + dir;
+    var index = path + '/index.json';
+
+    if (fs.existsSync(index)) {
+        output[dir] = require(index);
+        output[dir].path = path;
+    }
+});
