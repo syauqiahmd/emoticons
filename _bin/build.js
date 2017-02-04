c4f94448fe09a3487557ad60ccab46f9d7d80a43
Simon Schick <demwizzy@gmail.com>
2017-02-04 01:58:53 +0100

    es6ify this too

--- _bin/build.js
+++ _bin/build.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 
-var processing = require('../processing');
-var path = require('path');
-var fs = require('fs');
-var rm = require('rimraf');
+const processing = require('../processing');
+const path = require('path');
+const fs = require('fs');
+const rm = require('rimraf');
 
-var tasks = {
+const tasks = {
     /**
      * Runs tests on the target dir(s) to ensure the packs look
      * like they're valid.
      */
-    test: function (targets, argv) {
+    test (targets, argv) {
         targets.forEach(processing.validate);
     },
     /**
      * Builds sprite sheets for pack(s) and a manifest, outputting
      * to the --output dir.
      */
-    build: function (targets, argv) {
-        var manifest = {};
-        var todo = targets.length;
+    build (targets, argv) {
+        const manifest = {};
+        const todo = targets.length;
 
-        var dest = argv.build;
-        var size = parseInt(argv.size, 10) || 16;
+        const dest = argv.build;
+        const size = parseInt(argv.size, 10) || 16;
 
         rm.sync(dest);
         fs.mkdirSync(dest);
@@ -35,10 +35,10 @@ var tasks = {
                 );
             }
 
-            var target = targets[idx];
-            var file = path.join(dest, target + '.png');
+            const target = targets[idx];
+            const file = path.join(dest, target + '.png');
 
-            processing.spritesheet(size, target, file, function (err, m) {
+            processing.spritesheet(size, target, file, (err, m) => {
                 if (err) throw err;
                 manifest[target] = m;
                 next(idx + 1);
@@ -48,23 +48,17 @@ var tasks = {
 };
 
 function resolveTargets (argv) {
-    var target = argv._[0];
+    const target = argv._[0];
     if (argv.recursive) {
-        return fs.readdirSync(target).map(function (dir) {
-            return path.join(target, dir);
-        }).filter(function (dir) {
-            return fs.existsSync(path.join(dir, 'index.json'))
-        });
-    } else {
-        return [target];
+        return fs.readdirSync(target).map(dir => path.join(target, dir))
+        .filter(dir => fs.existsSync(path.join(dir, 'index.json')));
     }
+    return [target];
 }
 
-(function () {
-    var argv = require('minimist')(process.argv.slice(2));
-    for (var key in argv) {
-        if (key in tasks) {
-            tasks[key](resolveTargets(argv), argv);
-        }
+const argv = require('minimist')(process.argv.slice(2));
+for (let key in argv) {
+    if (key in tasks) {
+        tasks[key](resolveTargets(argv), argv);
     }
-})();
+}
